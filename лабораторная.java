202b3b72d9a1a573229b3fa814e475fc6a7911b7
EfremovaAnastasia <nastaefremova116@gmail.com>
2025-10-02 09:28:31 +0300

    код лабы

---
+++ лабораторная.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class лабораторная {
+    public static void main(String[] args) {
+        // Объявляем объект класса Scanner для ввода данных
+        Scanner in = new Scanner(System.in);
+        // Считываем с клавиатуры два целых числа
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a * (-1);
+        if (b==0)
+            System.out.println("Решения нет");
+        else
+            if ((a < 0 && b < 0) || (a > 0 && b > 0))  {
+                System.out.println("x <= " + "" + c + " "+ "and" + " "  + "x > 0");
+            } else {
+                if (a < 0 && b > 0) {
+                    System.out.println("x >= " + "" + c + " "+ "and" + " "  + "x < 0");
+                } else {
+                    System.out.println("x >= " + "" + c + " "+ "and" + " "  + "x < 0");
+            }
+        }
+    }
+}
